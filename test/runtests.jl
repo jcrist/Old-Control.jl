16c7c357cd2abf2af2d9ae6fbe15e4b6926ebe74
jcrist <crist042@umn.edu>
2014-03-26 17:21:44 -0500

    Improved test framework
    Now runs all tests before printing errors. Should be more
    informative, and useful for trouble shooting. Modeled after
    SymPy's test framework.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,81 @@ using Base.Test
 include("../src/Control.jl")
 using Control
 
-tests = ["transferfunction",
-         "statespace"]
+#############################################################################
+# Set up Custom Test Framework
+type TestData
+    npass::Integer
+    nfail::Integer
+    nerr::Integer
+    fail::Vector{Test.Failure}
+    error::Vector{Test.Error}
+end
+TestData() = TestData(0, 0, 0, Test.Failure[], Test.Error[])
 
+function custom_handler(r::Test.Success)
+    print("\033[92m.\033[0m")
+    TEST_DATA.npass += 1
+end
+
+function custom_handler(r::Test.Failure)
+    print("\033[91mF\033[0m")
+    TEST_DATA.nfail += 1
+    append!(TEST_DATA.fail, [r])
+end
+
+function custom_handler(r::Test.Error)
+    print("\033[93mE\033[0m")
+    TEST_DATA.nerr += 1
+    append!(TEST_DATA.error, [r])
+end
+
+info_string = """Julia Version: $(Base.VERSION)$(Base.commit_date)
+$(Base.commit_string)
+$(Sys.MACHINE)
+"""
+##############################################################################
+
+# List of tests
+path = Base.splitdir(Base.source_path())[1]
+tests = [splitext(x)[1] for x in filter(x->contains(x, "test_"), readdir(path))]
+
+# Initialize global test data struct
+global TEST_DATA = TestData()
+
+println(info_string)
+# Run the tests
 println("Running Tests:")
-for t in tests
-    println(" * $(t)")
-    include("$(t).jl")
+Test.with_handler(custom_handler) do
+    for t in tests
+        print(" * $(t): ")
+        include("$(t).jl")
+        print("\n")
+    end
+end
+print("\n")
+
+# Print failure and error information
+if TEST_DATA.nfail + TEST_DATA.nerr > 0
+    println("$(repeat("-", 80))")
+    for r in TEST_DATA.fail
+        println("\033[91mTest failed in:\033[0m $(r.expr)\n")
+    end
+    for r in TEST_DATA.error
+        println("\033[93mTest errored in:\033[0m $(r.expr)")
+        println(r.err)
+        Base.show_backtrace(STDOUT, :do_test, r.backtrace, 1:typemax(Int))
+        println("\n")
+    end
+end
+
+# Print test summary
+println(repeat("-", 80))
+println("Passed: $(TEST_DATA.npass), Failed: $(TEST_DATA.nfail), Errored: $(TEST_DATA.nerr)")
+println(repeat("-", 80))
+
+# Print if ok, in case above wasn't clear enough ;)
+if TEST_DATA.nfail + TEST_DATA.nerr == 0
+    println("OK TO COMMIT\n")
+else
+    println("DO NOT COMMIT\n")
 end
